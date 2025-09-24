4acdada89a9f3a5087a8dd874456b4d0a0f6ee8b
Wilhem Wundt Maxime <wilhemmaxime97@gmail.com>
2025-09-24 01:41:12 -0300

    Push in terminal

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const routers = require("./central.Routes.js")
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use("/api", routers)
 
 const port = process.env.PORT
